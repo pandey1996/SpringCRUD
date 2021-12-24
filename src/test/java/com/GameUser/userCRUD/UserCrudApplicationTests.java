248e527a4749212b18220af43c9c9f594fa7fb3b
Amit Pandey <amitpnd018@pop-os.localdomain>
2021-12-24 15:55:37 +0530

    Initial Commit

---
+++ src/test/java/com/GameUser/userCRUD/UserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GameUser.userCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
